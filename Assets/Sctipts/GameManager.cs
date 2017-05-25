48f571421040d089e353d83313c25b7fcd04fd63
owlaaz <owlaaz@outlook.com>
2017-05-25 16:53:59 +0700

    add new assets

--- Assets/Sctipts/GameManager.cs
+++ Assets/Sctipts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
@@ -27,93 +28,276 @@ public class GameManager : MonoBehaviour {
         if (s_instance == this) s_instance = null;
     }
 
-    public Button[] m_menuButton;
+    public RectTransform gardenMenuButtonsParent;
+    public RectTransform blockPanelGardenMenu;
+    public RectTransform blockPanelBubblePlantMenu;
+    public RectTransform blockPanelBubbleToolsMenu;
+    public RectTransform blockPanelInventoryTable;
+    public int currentGardenIndex;
+    public int inventoryCropboxStore = 0; // 0 : inventory , 1 Cropbox , 2 Store
+    public Transform[] gardenTranform;
+
+    //public Button[] gardenMenu;
+    public RectTransform[] gardenMenu;
+
+    public Transform[] gardens;
     
     void Start () {
         ResetGame();
         StartGame();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void StartGame()
     {
-
+        //menuButtons.gameObject.SetActive(true);
+        
     }
 
     public void ResetGame()
     {
 
     }
+    IEnumerator bubbleAnimation(RectTransform[] et,int n)
+    {
+        int j = 0;
+        foreach (RectTransform transform in et)
+        {
+            if (transform.gameObject.activeSelf)
+            {
+                transform.GetComponentInChildren<Animator>().SetTrigger("slide");
+                transform.GetComponentInChildren<Animator>().transform.parent.GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, -45*j + 45 * (n - 1)/2 );
+                yield return new WaitForSeconds(0.04f);
+                j++;
+            }
+        }
+    }
 
-    //public void nextDay()
-    //{
-    //    Farm.Instance.nextDay();
-    //}
-
-    //public void watering(int index)
-    //{
-    //    Farm.Instance.m_gardens[index].watering();
-    //}
-
-    //public void sellCrop()
-    //{
-    //    Farm.Instance.sellCrop();
-    //}
-    
     public void gardenOnClick(int index)
     {
-        foreach(Button b in m_menuButton)
+        int n = 0;
+        // check can plant
+        if (Farm.Instance.m_gardens[index].isEmpty() && Inventory.Instance.getBubbleButtonPlantList().Count > 0)
         {
-            b.onClick.RemoveAllListeners();
+            showGardenMenuButton(0, true);
+            n++;
+        }
+            
+        else
+        {
+            showGardenMenuButton(0, false);
+        }
+            
+        // check can watering
+        if (!Farm.Instance.m_gardens[index].isEmpty() && !Farm.Instance.m_gardens[index].isDied())
+        {
+            showGardenMenuButton(1, true);
+            n++;
+        }
+            
+        else
+        {
+            showGardenMenuButton(1, false);
+        }
+            
+        // check can harvest
+        if (!Farm.Instance.m_gardens[index].isEmpty() && Farm.Instance.m_gardens[index].isMature())
+        {
+            showGardenMenuButton(2, true);
+            n++;
+        }
+            
+        else
+        {
+            showGardenMenuButton(2, false);
+        }
+            
+        // check can remove
+        if (!Farm.Instance.m_gardens[index].isEmpty() && Farm.Instance.m_gardens[index].isDied())
+        {
+            showGardenMenuButton(3, true);
+            n++;
+        }
+            
+        else
+        {
+            showGardenMenuButton(3, false);
         }
 
-        int i = index;
-        m_menuButton[0].onClick.AddListener(() =>
+        // check can add tools
+        if(Inventory.Instance.getBubbleButtonToolsList().Count > 0)
         {
-            print("Plant Button Clicked");
-            // plant
-
-            //open inventory and return plant
-            //Plant r_plant = Farm.Instance.tomato;
-
-            // plant
-            //Farm.Instance.m_gardens[i].plant(r_plant);
-        });
-        m_menuButton[1].onClick.AddListener(() =>
+            showGardenMenuButton(4, true);
+            n++;
+        }
+        else
         {
-            print("Watering Button Clicked");
+            showGardenMenuButton(4, false);
+        }
+
+        if (n != 0)
+        {
+            currentGardenIndex = index;
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(gardens[index].position);
+
+            gardenMenuButtonsParent.transform.position = screenPosition;
+            gardenMenuButtonsParent.gameObject.SetActive(true);
+            blockPanelGardenMenu.gameObject.SetActive(true);
+
+
+            // play animation
+            StartCoroutine(bubbleAnimation(gardenMenu, n));
+
+            foreach (Transform et in gardenMenu)
+            {
+                et.GetComponentInChildren<EventTrigger>().triggers.Clear();
+            }
+
+
+
+            EventTrigger.Entry entry1 = new EventTrigger.Entry();
+            entry1.eventID = EventTriggerType.PointerClick;
+            entry1.callback.RemoveAllListeners();
+            entry1.callback.AddListener((eventData) =>
+            {
+                print("Plant Button Clicked");
+                // set posision
+                Inventory.Instance.bubbleButtonPlantParent.transform.position = screenPosition;
+                Inventory.Instance.bubbleButtonPlantParent.gameObject.SetActive(true);
+                StartCoroutine(bubbleAnimation(Inventory.Instance.getBubbleButtonPlantArray(), Inventory.Instance.getBubbleButtonPlantArray().Length));
+                hideGardenMenuButton();
+                blockPanelBubblePlantMenu.gameObject.SetActive(true);
+            });
+            gardenMenu[0].GetComponentInChildren<EventTrigger>().triggers.Add(entry1);
+
+            EventTrigger.Entry entry2 = new EventTrigger.Entry();
+            entry2.eventID = EventTriggerType.PointerClick;
+            entry2.callback.RemoveAllListeners();
+            entry2.callback.AddListener((eventData) =>
+            {
+                print("Watering Button Clicked");
+                hideGardenMenuButton();
             // watering
-            Farm.Instance.m_gardens[i].watering();
-        });
-        m_menuButton[2].onClick.AddListener(() =>
-        {
-            print("Harvest Button Clicked");
-            // harvest
-        });
-        m_menuButton[3].onClick.AddListener(() =>
-        {
-            print("Remove Plant Button Clicked");
-            // remove
-            Farm.Instance.m_gardens[i].removePlant();
-        });
-        m_menuButton[4].onClick.AddListener(() =>
-        {
-            print("Tools Button Clicked");
+            Farm.Instance.m_gardens[index].watering();
+
+            // change to dirt
+            gardenTranform[index].GetComponent<Renderer>().material = Resources.Load<Material>("materials/garden/watering garden");
+            });
+            gardenMenu[1].GetComponentInChildren<EventTrigger>().triggers.Add(entry2);
+
+            EventTrigger.Entry entry3 = new EventTrigger.Entry();
+            entry3.eventID = EventTriggerType.PointerClick;
+            entry3.callback.RemoveAllListeners();
+            entry3.callback.AddListener((eventData) =>
+            {
+                print("Harvest Button Clicked");
+                hideGardenMenuButton();
+                // harvest
+                Farm.Instance.m_gardens[index].harvestPlant();
+                // remove plant
+                Farm.Instance.m_gardens[index].removePlant();
+            });
+            gardenMenu[2].GetComponentInChildren<EventTrigger>().triggers.Add(entry3);
+
+            EventTrigger.Entry entry4 = new EventTrigger.Entry();
+            entry4.eventID = EventTriggerType.PointerClick;
+            entry4.callback.RemoveAllListeners();
+            entry4.callback.AddListener((eventData) =>
+            {
+                print("Remove Plant Button Clicked");
+                hideGardenMenuButton();
+                // remove plant
+                Farm.Instance.m_gardens[index].removePlant();
+            });
+            gardenMenu[3].GetComponentInChildren<EventTrigger>().triggers.Add(entry4);
+
+            EventTrigger.Entry entry5 = new EventTrigger.Entry();
+            entry5.eventID = EventTriggerType.PointerClick;
+            entry5.callback.RemoveAllListeners();
+            entry5.callback.AddListener((eventData) =>
+            {
+                print("Tools Button Clicked");
+                Inventory.Instance.bubbleButtonToolsParent.transform.position = screenPosition;
+                Inventory.Instance.bubbleButtonToolsParent.gameObject.SetActive(true);
+                StartCoroutine(bubbleAnimation(Inventory.Instance.getBubbleButtonToolsArray(), Inventory.Instance.getBubbleButtonToolsArray().Length));
+                hideGardenMenuButton();
+                blockPanelBubbleToolsMenu.gameObject.SetActive(true);
             // tools
             //open inventory and return tools
             //Tools r_tool = Farm.Instance.Poop;
 
             //add to garden
             //Farm.Instance.m_gardens[i].addTool(r_tool);
-        });
+            });
+            gardenMenu[4].GetComponentInChildren<EventTrigger>().triggers.Add(entry5);
+        }
     }
 
+    private void showGardenMenuButton(int index,bool toggle)
+    {
+        gardenMenu[index].gameObject.SetActive(toggle);
+    }
+
+    public void ShowInventoryTable()
+    {
+        inventoryCropboxStore = 0;
+        //show inventory table
+        Inventory.Instance.itemInventoryParent.gameObject.SetActive(true);
+
+        blockPanelInventoryTable.gameObject.SetActive(true);
+    }
+    public void showCropboxTable()
+    {
+        inventoryCropboxStore = 1;
+        Inventory.Instance.itemInventoryParent.gameObject.SetActive(true);
+
+        blockPanelInventoryTable.gameObject.SetActive(true);
+    }
+    public void showStoreTable()
+    {
+        inventoryCropboxStore = 2;
+    }
+    public void hideGardenMenuButton()
+    {
+        foreach(Transform p_button in gardenMenu)
+        {
+            if(p_button.gameObject.activeSelf == true)
+            {
+                p_button.GetComponentInChildren<Animator>().SetTrigger("hide");
+            }
+        }
+        blockPanelGardenMenu.gameObject.SetActive(false);
+    }
+    public void hideBubblePlantButton()
+    {
+        foreach (Transform p_button in Inventory.Instance.getBubbleButtonPlantList())
+        {
+            if (p_button.gameObject.activeSelf == true)
+            {
+                p_button.GetComponentInChildren<Animator>().SetTrigger("hide");
+            }
+        }
+        blockPanelBubblePlantMenu.gameObject.SetActive(false);
+    }
+    public void hideBubbleToolsButton()
+    {
+        foreach (Transform p_button in Inventory.Instance.getBubbleButtonToolsList())
+        {
+            if (p_button.gameObject.activeSelf == true)
+            {
+                p_button.GetComponentInChildren<Animator>().SetTrigger("hide");
+            }
+        }
+        blockPanelBubbleToolsMenu.gameObject.SetActive(false);
+    }
+    public void hideInventoryTable()
+    {
+        // play animation
 
 
+        // close
+        Inventory.Instance.itemInventoryParent.gameObject.SetActive(false);
+        blockPanelInventoryTable.gameObject.SetActive(false);
+    }
 }
 
 
--- Assets/Sctipts/Garden.cs
+++ Assets/Sctipts/Garden.cs
@@ -2,35 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [Serializable]
 public class Garden
 {
-
+    private Transform gardenTranfrom;
     [SerializeField]
-    private bool empty;
+    private bool empty = true;
     [SerializeField]
-    private Plant m_plant;
+    private Plant m_plant = null;
     [SerializeField]
-    private int plantDay;
+    private int plantDay=0;
     [SerializeField]
-    private int dayToGrowth;
+    private int dayToGrowth=0;
     [SerializeField]
-    private int dayToDied;
+    private int dayToDied=0;
     [SerializeField]
-    private List<Tools> listOfTool;
+    private List<Tools> listOfTool = new List<Tools>();
     [SerializeField]
-    private bool water;
+    private bool water=false;
 
     public Garden()
     {
-        empty = true;
-        m_plant = null;
-        plantDay = 0;
-        dayToGrowth = 0;
-        dayToDied = 0;
-        listOfTool = new List<Tools>();
-        water = false;
+        //empty = true;
+        //m_plant = null;
+        //plantDay = 0;
+        //dayToGrowth = 0;
+        //dayToDied = 0;
+        //listOfTool = new List<Tools>();
+        //water = false;
     }
 
     public void plant(Plant p_plant)
@@ -53,44 +54,131 @@ public class Garden
     {
         int get_level = m_plant.getPlantLevel();
         plantDay++;
-        if (listOfTool.Count > 0)
+        foreach(Tools pop_tool in listOfTool)
         {
-            Tools pop_tool = listOfTool[listOfTool.Count - 1];
-            do
-            {
-                dayToGrowth -= pop_tool.getEffectToGrowth();
-                dayToDied -= pop_tool.getEffectToDied();
-                listOfTool.Remove(pop_tool);
-                pop_tool = listOfTool[listOfTool.Count - 1];
-            } while (listOfTool.Count > 0);
+            dayToGrowth -= pop_tool.getEffectToGrowth();
+            dayToDied -= pop_tool.getEffectToDied();
         }
+        listOfTool.Clear();
         if (!m_plant.isMature())
         {
             dayToDied--;
             if (dayToDied <= 0)
+            {
+                //hide seed
+                gardenTranfrom.GetChild(0).gameObject.SetActive(false);
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        //change image
+                        gardenTranfrom.GetChild(i * 3 + j + 1).GetComponent<Renderer>().material = Resources.Load<Material>("materials/plants/died plant");
+                        //show poop
+                        gardenTranfrom.GetChild(i * 3 + j + 1).gameObject.SetActive(true);
+                        // rescale
+                        gardenTranfrom.GetChild(i * 3 + j + 1).transform.localScale = new Vector3(0.25f, 1, 0.1f);
+                        gardenTranfrom.GetChild(i * 3 + j + 1).transform.localPosition = new Vector3((i - 1) * 3, 0.2f, (j - 1) * 3);
+                    }
+                }
                 m_plant.died();
+            }
+                
             if (water == true) dayToGrowth--;
-            if (dayToGrowth <= 0)
+            if (dayToGrowth <= 0 && !isDied())
             {
                 m_plant.growth();
                 int new_level = m_plant.getPlantLevel();
 
                 dayToGrowth = m_plant.getDayToGrowth(new_level);
                 dayToDied = m_plant.getDayToDied(new_level);
+
+                for(int i=0;i<3;i++)
+                {
+                    for(int j=0;j<3;j++)
+                    {
+                        //garden change image
+                        gardenTranfrom.GetChild(i * 3 + j + 1).GetComponent<Renderer>().material = Resources.Load<Material>("materials/plants/" + m_plant.getItemName().Replace(" seed", "") + " plant");
+
+                        // set plant active true
+                        gardenTranfrom.GetChild(i * 3 + j + 1).gameObject.SetActive(true);
+
+                        // change scale
+                        gardenTranfrom.GetChild(i * 3 + j + 1).transform.localScale = new Vector3(0.4f / m_plant.getMaxLevel() * m_plant.getPlantLevel(), 1, 0.16f / m_plant.getMaxLevel() * m_plant.getPlantLevel());
+                        gardenTranfrom.GetChild(i * 3 + j + 1).transform.localPosition = new Vector3((i - 1) * 3, 0.8f / m_plant.getMaxLevel() * m_plant.getPlantLevel(), (j - 1) * 3);
+                    }
+                }
+
+                // hide seed
+                gardenTranfrom.GetChild(0).gameObject.SetActive(false);
             }
         }
         else
         {
             if (dayToDied <= 0)
+            {
+                //hide seed
+                gardenTranfrom.GetChild(0).gameObject.SetActive(false);
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        //change image
+                        gardenTranfrom.GetChild(i * 3 + j + 1).GetComponent<Renderer>().material = Resources.Load<Material>("materials/plants/died plant");
+                        //show poop
+                        gardenTranfrom.GetChild(i * 3 + j + 1).gameObject.SetActive(true);
+                        // rescale
+                        gardenTranfrom.GetChild(i * 3 + j + 1).transform.localScale = new Vector3(0.25f, 1, 0.1f);
+                        gardenTranfrom.GetChild(i * 3 + j + 1).transform.localPosition = new Vector3((i - 1) * 3, 0.2f, (j - 1) * 3);
+                    }
+                }
                 m_plant.died();
+            }
             dayToDied--;
-            if (water == true) dayToDied = m_plant.getDayToDied(get_level);
+            if (water == true && !isDied()) dayToDied = m_plant.getDayToDied(get_level);
 
         }
         water = false;
+        // dry dirt
+        gardenTranfrom.GetComponent<Renderer>().material = Resources.Load<Material>("materials/garden/dry garden");
+    }
+    public void harvestPlant()
+    {
+        Inventory.Instance.addItemToList(m_plant.getItemID() + 2000, 9);
     }
     public void removePlant()
     {
+        // set gardet empty
         empty = true;
+
+        // remove seed
+        gardenTranfrom.GetChild(0).gameObject.SetActive(false);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                // hide plant
+                gardenTranfrom.GetChild(i * 3 + j + 1).gameObject.SetActive(false);
+            }
+        }
+    }
+    public bool isEmpty()
+    {
+        return empty;
+    }
+    public bool isMature()
+    {
+        return m_plant.isMature();
+    }
+    public void setGardenTransform(Transform p_transform)
+    {
+        gardenTranfrom = p_transform;
+    }
+    public bool isDied()
+    {
+        if (m_plant.getPlantLevel() < 0 || dayToDied < 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
